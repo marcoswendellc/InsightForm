bdebf3d47d3e997f53f3e37bc9b1604a190a264b
marcoswendellc <marcoswendellc@hotmail.com>
2026-04-13 23:55:03 -0300

    Inserindo espaçamento no tipo tamanho

--- src/pages/FormBuilder/components/QuestionPreview.tsx
+++ src/pages/FormBuilder/components/QuestionPreview.tsx
@@ -765,24 +765,27 @@ export default function QuestionPreview({
                       />
                     </PreviewSizeField>
 
-                    <PreviewSizeInput
-                      type="number"
-                      step="0.01"
-                      min="0"
-                      max="9999.99"
-                      value={optionSize.width}
-                      onChange={(e) => {
-                        let value = e.target.value;
+                    <PreviewSizeField>
+                      <PreviewSizeFieldLabel>Largura</PreviewSizeFieldLabel>
+                      <PreviewSizeInput
+                        type="number"
+                        step="0.01"
+                        min="0"
+                        max="9999.99"
+                        value={optionSize.width}
+                        onChange={(e) => {
+                          let value = e.target.value;
 
-                        // bloqueia valores fora do padrão
-                        if (/^\d{0,4}(\.\d{0,2})?$/.test(value) || value === "") {
-                          updateCheckboxSize(option.id, "width", value);
-                        }
-                      }}
-                      onBlur={() => blurCheckboxSize(option.id, "width")}
-                      disabled={disabled}
-                      placeholder="Ex.: 1.20"
-                    />
+                          // bloqueia valores fora do padrão
+                          if (/^\d{0,4}(\.\d{0,2})?$/.test(value) || value === "") {
+                            updateCheckboxSize(option.id, "width", value);
+                          }
+                        }}
+                        onBlur={() => blurCheckboxSize(option.id, "width")}
+                        disabled={disabled}
+                        placeholder="Ex.: 1.20"
+                      />
+                    </PreviewSizeField>
                     
                     <PreviewSizeField>
                       <PreviewSizeFieldLabel>Unidade</PreviewSizeFieldLabel>
